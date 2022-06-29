72c7a4aea8c98f9d6943460939d2ff16aeda98ba
Mohammad Sahil <sahilmejakhas@gmail.com>
2022-06-29 20:04:56 +0530

    heroku deploy

--- public/script.js
+++ public/script.js
@@ -2,7 +2,7 @@ const socket = io('/')
 const videoGrid = document.getElementById('video-grid');
 
 const myPeer = new Peer(undefined, {
-  // path: '/',
+  path: '/peerjs',
   host: '/',
   port: '443'
 })
