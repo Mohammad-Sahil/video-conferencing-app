95cfa68c49ab075330359310dce10294eefeae97
Mohammad Sahil <sahilmejakhas@gmail.com>
2022-06-29 18:48:22 +0530

    text feature added

--- public/script.js
+++ public/script.js
@@ -10,12 +10,18 @@ const myPeer = new Peer(undefined, {
 const myVideo = document.createElement('video');
 myVideo.muted = true;
 const peers = {}
+
+let myVideoStream;
+
 navigator.mediaDevices.getUserMedia({
   video: true,
   audio: true
 }).then(stream => {
+  myVideoStream = stream;
   addVideoStream(myVideo, stream);
-  console.log('first code executed')
+
+
+  // answer the incoming user call
   myPeer.on('call', call => {
     call.answer(stream);
     const video = document.createElement('video');
@@ -67,4 +73,89 @@ const addVideoStream = (video, stream) => {
     video.play();
   });
   videoGrid.append(video)
+}
+
+
+
+
+let msg = $('input')
+
+// using jQuery to send message
+$('html').keydown((e) => {
+  // on press of ENTER KEY send message
+  // clear the message as soon as ENTER is pressed
+  if(e.which == 13 && msg.val().length !== 0) {
+    console.log(msg.val())
+    socket.emit('message', msg.val());
+    msg.val('')
+  }
+});
+
+// receive message from server
+socket.on('createMessage', message => {
+  console.log('this is another message', message)
+  $('ul').append(`<li class="message"><b>user</b><br/>${message}</li>`);
+  // scrollMessages()
+})
+ 
+// const scrollMessages = () => {
+//   var d = $('.main__chat_window');
+//   d.scrollMessages(d.prop("scrollHeight"))
+// }
+
+// Mute Our Video
+const muteUnmute = () => {
+  const enabled = myVideoStream.getAudioTracks()[0].enabled;
+  if(enabled) {
+    myVideoStream.getAudioTracks()[0].enabled = false;
+    setUnmuteButton();
+  }
+  else {
+    setMuteButton();
+    myVideoStream.getAudioTracks()[0].enabled = true;
+  }
+}
+
+const setMuteButton = () => {
+  const html = `
+    <i class="fas fa-microphone"></i>
+    <span>Mute</span>
+  `
+  document.querySelector('.main__mute_button').innerHTML=html;
+}
+
+const setUnmuteButton = () => {
+  const html = `
+    <i class="unmute fas fa-microphone-slash"></i>
+    <span>Unmute</span>
+  `
+  document.querySelector('.main__mute_button').innerHTML=html;
+} 
+
+const playStop = () => {
+  let enabled = myVideoStream.getVideoTracks()[0].enabled;
+  if(enabled) {
+    myVideoStream.getVideoTracks()[0].enabled=false;
+    setPlayVideo()
+  }
+  else {
+    setStopVideo()
+    myVideoStream.getVideoTracks()[0].enabled=true;
+  }
+}
+
+const setStopVideo = () => {
+  const html = `
+    <i class="fas fa-video"></i>
+    <span>Stop Video</span>
+  `
+  document.querySelector(".main__video_button").innerHTML=html;
+}
+
+const setPlayVideo = () => {
+  const html = `
+    <i class="stop fas fa-video-slash"></i>
+    <span>Play Video</span>
+  `
+  document.querySelector(".main__video_button").innerHTML=html;
 }
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const server = require("http").Server(app);
 const io = require("socket.io")(server);
 const ejs = require('ejs');
 const { v4: uuidv4 } = require('uuid');
+const PORT = process.env.PORT || 8000;
 const { ExpressPeerServer } = require("peer");
 const peerServer = ExpressPeerServer(server, {
   debug: true,
@@ -32,9 +33,15 @@ io.on('connection', socket => {
        socket.on('disconnect', () => {
         socket.to(roomId).emit('user-disconnected', userId);
        })
+
+       // listen and recieve message sent from other user and send back to same room
+       socket.on('message', message => {
+        console.log('this is another message 2', message)
+        io.to(roomId).emit('createMessage', message)
+      })
     })
 })
 
-server.listen(8000, (req,res) => {
-    console.log(`Server successfully connected to port 8000`)
+server.listen(PORT, (req,res) => {
+    console.log(`Server successfully connected to port ${PORT}`)
 })
